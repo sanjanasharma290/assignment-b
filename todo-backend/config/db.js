c02efbd40e440d62fd937da7349ff836029d43a5
sanjanasharma290 <sanjanasharma290s@gmail.com>
2025-07-28 16:43:33 +0530

    done

--- todo-backend/config/db.js
+++ todo-backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Load env variables
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB Connection Error:', error.message);
